8f3d1aec88e94acd98a0fe61123ebe505d36a495
土屋絵里佳 <>
2025-07-19 22:19:01 +0900

    変更

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,40 +1,32 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const buttons = document.querySelectorAll(".accordion-btn");
-  buttons.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      const accordion = btn.closest(".accordion");
-      const content = btn.nextElementSibling;
-      const isOpen = accordion.classList.contains("is-open");
-      document
-        .querySelectorAll(".accordion.is-open")
-        .forEach((openAccordion) => {
-          if (openAccordion !== accordion) {
-            openAccordion.classList.remove("is-open");
-            openAccordion.querySelector(".accordion-btn");
-            openAccordion.querySelector(".accordion-content").style.maxHeight =
-              null;
-          }
-        });
-      if (isOpen) {
-        accordion.classList.remove("is-open");
-        content.style.maxHeight = null;
-      } else {
-        accordion.classList.add("is-open");
-        content.style.maxHeight = content.scrollHeight + "px";
+const buttons = document.querySelectorAll(".accordion-btn");
+buttons.forEach((btn) => {
+  btn.addEventListener("click", () => {
+    const accordion = btn.closest(".accordion");
+    const content = btn.nextElementSibling;
+    const isOpen = accordion.classList.contains("is-open");
+    document.querySelectorAll(".accordion.is-open").forEach((openAccordion) => {
+      if (openAccordion !== accordion) {
+        openAccordion.classList.remove("is-open");
+        openAccordion.querySelector(".accordion-btn");
+        openAccordion.querySelector(".accordion-content").style.maxHeight = 0;
       }
     });
-  });
-  document.addEventListener("click", (event) => {
-    const clickedInsideAccordion = event.target.closest(".accordion");
-    if (!clickedInsideAccordion) {
-      document
-        .querySelectorAll(".accordion.is-open")
-        .forEach((openAccordion) => {
-          openAccordion.classList.remove("is-open");
-          openAccordion.querySelector(".accordion-btn");
-          openAccordion.querySelector(".accordion-content").style.maxHeight =
-            null;
-        });
+    if (isOpen) {
+      accordion.classList.remove("is-open");
+      content.style.maxHeight = 0;
+    } else {
+      accordion.classList.add("is-open");
+      content.style.maxHeight = content.scrollHeight + "px";
     }
   });
+});
+document.addEventListener("click", (event) => {
+  const clickedInsideAccordion = event.target.closest(".accordion");
+  if (!clickedInsideAccordion) {
+    document.querySelectorAll(".accordion.is-open").forEach((openAccordion) => {
+      openAccordion.classList.remove("is-open");
+      openAccordion.querySelector(".accordion-btn");
+      openAccordion.querySelector(".accordion-content").style.maxHeight = 0;
+    });
+  }
 });
