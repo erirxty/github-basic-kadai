2bf5979ab7d383d38f4352ff04bd80068a84cdc6
土屋絵里佳 <>
2025-07-19 22:16:24 +0900

    変更

--- tabmenu/tab.js
+++ tabmenu/tab.js
@@ -1,26 +1,24 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const tabButtons = document.querySelectorAll(".tab-button");
-  const tabPanes = document.querySelectorAll(".tab-pane");
+const tabButtons = document.querySelectorAll(".tab-button");
+const tabPanes = document.querySelectorAll(".tab-pane");
 
-  tabButtons.forEach((button) => {
-    button.addEventListener("click", () => {
-      const currentActiveButton = document.querySelector(".tab-button.active");
-      const currentActivePane = document.querySelector(".tab-pane.active");
+tabButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    const currentActiveButton = document.querySelector(".tab-button.active");
+    const currentActivePane = document.querySelector(".tab-pane.active");
 
-      if (currentActiveButton) {
-        currentActiveButton.classList.remove("active");
-      }
-      if (currentActivePane) {
-        currentActivePane.classList.remove("active");
-      }
+    if (currentActiveButton) {
+      currentActiveButton.classList.remove("active");
+    }
+    if (currentActivePane) {
+      currentActivePane.classList.remove("active");
+    }
 
-      button.classList.add("active");
-      const targetTabId = button.dataset.tab;
-      const targetTabPane = document.getElementById(targetTabId);
+    button.classList.add("active");
+    const targetTabId = button.dataset.tab;
+    const targetTabPane = document.getElementById(targetTabId);
 
-      if (targetTabPane) {
-        targetTabPane.classList.add("active");
-      }
-    });
+    if (targetTabPane) {
+      targetTabPane.classList.add("active");
+    }
   });
 });
