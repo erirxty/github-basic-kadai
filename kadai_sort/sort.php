d4991d3296a7588ebaba359a07f2a3b4436cb014
土屋絵里佳 <>
2025-08-02 14:00:26 +0900

    追加

---
+++ kadai_sort/sort.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP基礎編</title>
+</head>
+<body>
+    <p>
+        <?php
+        
+        function sort_2way($array, $order) { 
+            if ($order === TRUE) {
+                sort($array); 
+                echo "昇順ソートします。<br>";
+            } else {
+                rsort($array); 
+                echo "降順ソートします。<br>";
+            }
+            foreach ($array as $value) { /
+                echo $value . "<br>"; 
+            }
+            echo "<br>"; 
+        }
+        $nums= [15, 4, 18, 23, 10];
+        sort_2way($nums, TRUE);
+        $num = [15, 4, 18, 23, 10];
+        sort_2way($nums, FALSE);
+        ?>
+    </p>
+</body>s
+</html>
